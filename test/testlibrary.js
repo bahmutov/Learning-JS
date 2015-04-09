c5c3400ec8217f8126ddfdc3ceaa07a8d5ca2309
Lindsey <lrraymond13@gmail.com>
2015-04-09 18:16:51 -0400

    closes #2

--- test/testlibrary.js
+++ test/testlibrary.js
@@ -2,7 +2,7 @@
 
 var test = require('unit.js');
 
-var library = require('library_fnc');
+var library = require('../library_fnc');
 
 
 describe('mocha+assert API description', function() {
@@ -16,39 +16,39 @@ var library = function(testype, testval) {
     var a = 1;
     var b = [1,2,3];
     var c = null;
-    assert.equal(library('NUMBER', a), true);
-    assert.equal(library('number', b), false);
-    assert.equal(library('number', c), false);
+    test.assert.equal(library('NUMBER', a), true);
+    test.assert.equal(library('number', b), false);
+    test.assert.equal(library('number', c), false);
   });
   it('strings equal?', function() {
     var a = 'stri';
     var b = true;
     var c = null;
-    assert.equal(library('string', a), true);
-    assert.equal(library('string', b), false);
-    assert.equal(library('string', c), false);
+    test.assert.equal(library('string', a), true);
+    test.assert.equal(library('string', b), false);
+    test.assert.equal(library('string', c), false);
   });
   
   it('boolean', function() {
     var a = true;
     var b = 'not boolean';
     var c = null;
-    assert.equal(library('boolean', a), true);
-    assert.equal(library('boolean', c), false);
+    test.assert.equal(library('boolean', a), true);
+    test.assert.equal(library('boolean', c), false);
   });
   
   it('array', function() {
     var o = [1,2,3,4];
     var r = /ab+c/;
     var c = null;
-    assert.equal(library('array', o), true);
-    assert.equal(library('array', r), false);
-    assert.equal(library('array', c), false);
+    test.assert.equal(library('array', o), true);
+    test.assert.equal(library('array', r), false);
+    test.assert.equal(library('array', c), false);
   });
   it('regexp', function() {
     var r = /ab+c/;
     var o = [1,2,3,4]
-    assert.equal(library('array', o), true);
-    assert.equal(library('array', r), false);
+    test.assert.equal(library('array', o), true);
+    test.assert.equal(library('array', r), false);
   });
   });
